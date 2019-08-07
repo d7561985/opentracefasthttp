7182e06e65c20264b57a80d81c045bcd130bbc97
dzmitryharupa <dzmitry.harupa@1pt.com>
2019-08-07 11:28:43 +0300

    test: rid from jaeger requirement for tests.

--- fasthttp_test.go
+++ fasthttp_test.go
@@ -1,14 +1,8 @@
-// +build jaeger
-
-// test
 package opentracefasthttp_test
 
 import (
-	"fmt"
 	"net"
-	"net/http"
 	"testing"
-	"time"
 
 	"github.com/d7561985/opentracefasthttp"
 	"github.com/opentracing/opentracing-go"
@@ -16,27 +10,15 @@ import (
 	"github.com/opentracing/opentracing-go/log"
 	"github.com/stretchr/testify/assert"
 	"github.com/uber/jaeger-client-go"
-	"github.com/uber/jaeger-client-go/config"
 	"github.com/valyala/fasthttp"
 	"github.com/valyala/fasthttp/fasthttputil"
 )
 
-const JaegerURL = "http://localhost:16686"
-
 func TestCarrier(t *testing.T) {
-	waitJaeger()
+	tracer, closer := jaeger.NewTracer("fasthttp-carrier-tester", jaeger.NewConstSampler(true), jaeger.NewNullReporter())
+	defer closer.Close()
 
-	// we need real trace connection
-	cfg, err := config.FromEnv()
-	assert.NoError(t, err)
-	cfg.ServiceName = "fasthttp-carrier-tester"
-	cfg.Sampler.Type = "const"
-	cfg.Sampler.Param = 1
-
-	tr, cl, err := cfg.NewTracer(config.Logger(jaeger.StdLogger))
-	assert.NoError(t, err)
-	defer cl.Close() //nolint
-	opentracing.SetGlobalTracer(tr)
+	opentracing.SetGlobalTracer(tracer)
 
 	ok := false
 	ln := fasthttputil.NewInmemoryListener()
@@ -44,9 +26,9 @@ func TestCarrier(t *testing.T) {
 
 	srv := fasthttp.Server{Handler: func(ctx *fasthttp.RequestCtx) {
 		carrier := opentracefasthttp.New(&ctx.Request.Header)
-		clientContext, err := tr.Extract(opentracing.HTTPHeaders, carrier)
+		clientContext, err := tracer.Extract(opentracing.HTTPHeaders, carrier)
 		assert.NoError(t, err)
-		span := tr.StartSpan("HTTP "+string(ctx.Method())+" "+ctx.Request.URI().String(), ext.RPCServerOption(clientContext))
+		span := tracer.StartSpan("HTTP "+string(ctx.Method())+" "+ctx.Request.URI().String(), ext.RPCServerOption(clientContext))
 		assert.NotNil(t, span)
 		span.LogFields(log.String("server", "request ok"))
 		defer span.Finish()
@@ -66,7 +48,7 @@ func TestCarrier(t *testing.T) {
 	req.SetRequestURI("http://example.com")
 
 	carrier := opentracefasthttp.New(&req.Header)
-	err = opentracing.GlobalTracer().Inject(span.Context(), opentracing.HTTPHeaders, carrier)
+	err := opentracing.GlobalTracer().Inject(span.Context(), opentracing.HTTPHeaders, carrier)
 	assert.NoError(t, err)
 
 	client := fasthttp.Client{Dial: func(addr string) (net.Conn, error) {
@@ -76,33 +58,4 @@ func TestCarrier(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.True(t, ok)
-}
-
-func waitJaeger() {
-	res := make(chan struct{})
-	go checkJaeger(res)
-
-	select {
-
-	case <-time.After(time.Minute):
-		panic("no connection")
-	case <-res:
-		fmt.Println("Jaeger has found")
-		return
-	}
-}
-
-func checkJaeger(res chan<- struct{}) {
-	request, err := http.NewRequest("GET", JaegerURL, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	for {
-		if response, err := http.DefaultClient.Do(request); err == nil && response.StatusCode == http.StatusOK {
-			res <- struct{}{}
-			return
-		}
-		time.Sleep(time.Second)
-	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/d7561985/opentracefasthttp
 go 1.12
 
 require (
+	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
 	github.com/opentracing/opentracing-go v1.1.0
-	github.com/pkg/errors v0.8.1 // indirect
 	github.com/stretchr/testify v1.3.0
 	github.com/uber/jaeger-client-go v2.16.0+incompatible
 	github.com/uber/jaeger-lib v2.0.0+incompatible // indirect
